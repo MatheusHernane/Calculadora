292fbdc544e45313f21991d7d63d4ee5b6c70599
Matheus <matheussena.engmec@gmail.com>
2023-09-18 20:58:53 -0300

    Instalando biblioteca de Botão

---
+++ calculadora/src/components/botao.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@mui/material-next/Button";
+
+const botao = () => {
+  return (
+    <div>
+      <h1>Botão</h1>
+    </div>
+  );
+};
+
+export default botao;
