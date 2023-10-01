e19d8f530ddbd07624210200f07a154d53ad5587
Matheus <matheussena.engmec@gmail.com>
2023-10-01 19:11:06 -0300

    Corrigindo layout da calculadora

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,6 +1,7 @@
 import { Box, Container } from "@mui/material";
 import "./App.css";
 import Botao from "./components/botao";
+import Calculadora from "./components/calculadora";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>Calculadora UI</h1>
       <Container maxWidth="xs">
         <Box sx={{ bgcolor: "#fff", height: "300px" }}>
+          <Calculadora />
           <Botao />
         </Box>
       </Container>
