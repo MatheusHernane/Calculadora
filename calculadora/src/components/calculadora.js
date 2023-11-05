a419d9d47a5612296a3c37f814c47af5acc4aae4
Matheus <matheussena.engmec@gmail.com>
2023-11-05 09:57:11 -0300

    resolvendo conflitos 2

--- calculadora/src/components/calculadora.js
+++ calculadora/src/components/calculadora.js
@@ -24,53 +24,7 @@ const theme = createTheme({
 });
 
 export default function Calculadora() {
-<<<<<<< HEAD
   const { num, inputNum, clear, operation, calculate } = useCalculator();
-=======
-  const [num, setNum] = useState(0);
-  const [oldnum, setOldnum] = useState(0);
-  const [operator, setOperator] = useState();
-
-  function inputNum(e) {
-    let input = e.target.value;
-    if (num === 0) {
-      setNum(input);
-    } else {
-      setNum(num + input);
-    }
-  }
-
-  function clear() {
-    setNum(0);
-    setOldnum(0);
-    setOperator(0);
-  }
-
-  function operation(e) {
-    var operatorInput = e.target.value;
-    setOperator(operatorInput);
-    setOldnum(num);
-    setNum(0);
-  }
-
-  function calculate() {
-    if (operator === "/") {
-      setNum(parseFloat(oldnum) / parseFloat(num)); //parseFloat converte a string em número
-    } else if (operator === "x") {
-      setNum(parseFloat(oldnum) * parseFloat(num));
-    } else if (operator === "+") {
-      setNum(parseFloat(oldnum) + parseFloat(num));
-    } else if (operator === "-") {
-      setNum(parseFloat(oldnum) - parseFloat(num));
-    } else if (operator === "x²") {
-      setNum(parseFloat(oldnum) * parseFloat(oldnum));
-    } else if (operator === "x³") {
-      setNum(parseFloat(oldnum) * parseFloat(oldnum) * parseFloat(oldnum));
-    } else if (operator === "√") {
-      setNum(Math.sqrt(parseFloat(oldnum)));
-    }
-  }
->>>>>>> 15fe690a0ebc7ae1f9f77ba293e4f8bfbea03725
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ "& button": { m: 0 } }}>
